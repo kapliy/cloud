c4e4aa0c5acd2a81aae74321d044c6faa02c1f68
Anton Kapliy <kapliy@gmail.com>
2015-04-27 09:35:25 -0400

    mount /bulkinsert

--- aam-lite/launch.sh
+++ aam-lite/launch.sh
@@ -1,2 +1,2 @@
 USER=$(logname)
-docker run -it --rm -v /home/$USER/:/mnt/home:ro -v /opt/anaconda:/opt/anaconda:ro kapliy/aam-lite
+docker run -it --rm -v /home/$USER/:/mnt/home:ro -v /bulkinsert:/bulkinsert:rw -v /opt/anaconda:/opt/anaconda:ro kapliy/aam-lite
