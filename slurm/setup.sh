c3f946cd0e52c56f39920c00142292dff27e525f
Anton Kapliy <kapliy@gmail.com>
2015-05-02 10:45:42 -0400

    updates for required pacakges & slurm configuration

--- slurm/setup.sh
+++ slurm/setup.sh
@@ -1,3 +1,3 @@
 
-# sudo apt-get install mysql-server libmysqlclient-dev libcurl-dev man2html
+# sudo apt-get install mysql-server libmysqlclient-dev libcurl-dev man2html libcurl4-openssl-dev
 ./configure --prefix=/opt/slurm/release --sysconfdir=/opt/slurm/release/etc
